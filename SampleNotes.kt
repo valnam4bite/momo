df1283238417bc1830d5b4ef29cf12055f8a5429
valnam4bite <valnam@ukr.net>
2026-07-21 20:56:19 +0300

    Create SampleNotes.kt

---
+++ SampleNotes.kt
@@ -0,0 +1,65 @@
+object SampleNotes {
+
+    fun load() = listOf(
+
+        Note(
+
+            "Kotlin Basics",
+
+            "# Kotlin\nSee [[Kotlin Collections]]",
+
+            listOf(
+
+                "kotlin",
+
+                "programming"
+
+            )
+
+        ),
+
+        Note(
+
+            "Kotlin Collections",
+
+            "Lists, Sets and Maps.",
+
+            listOf(
+
+                "kotlin"
+
+            )
+
+        ),
+
+        Note(
+
+            "Markdown",
+
+            "Formatting guide",
+
+            listOf(
+
+                "markdown"
+
+            )
+
+        ),
+
+        Note(
+
+            "Algorithms",
+
+            "Sorting examples",
+
+            listOf(
+
+                "algorithms"
+
+            )
+
+        )
+
+    )
+
+}
